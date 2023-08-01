e5477c95f8d5053e79c0fd0a9762ed7b4c01d9af
mykola_umrikhin <umrihin2611@gmail.com>
2023-08-01 13:57:57 +0300

    Done mobile version

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,67 @@
 // console.log("TEST");
+// //START MOBILE Menu
+const menuBtn = $(".menu-button");
+const darkOverlay = $(".dark-overlay");
+const mobileMenu = $(".mobile-menu");
 
+$(document).on("click", ".menu-button", handleMenu);
+$(document).on("click", ".is-submenu", handleToggleMenu);
+
+function handleMenu(e) {
+  e.preventDefault();
+  darkOverlay.toggleClass("visible");
+  mobileMenu.toggleClass("visible");
+}
+
+function handleToggleMenu(e) {
+  e.preventDefault();
+  const $this = $(this);
+  $this
+    .parent(".has-submenu")
+    .toggleClass("opened")
+    .siblings("li")
+    .removeClass("opened")
+    .find("ul")
+    .hide();
+
+  $this.next("ul").slideToggle(500, function () {
+    $(this).find(".has-submenu").removeClass("opened").children("ul").hide();
+  });
+}
+
+function resetMobileMenu() {
+  darkOverlay.removeClass("visible");
+  mobileMenu.removeClass("visible");
+  $(".mobile-navigation .has-submenu").removeClass("opened").find("ul").hide();
+}
+
+/* =======================  */
+
+function initMobile() {
+  console.log("is-mobile");
+}
+
+function initDesktop() {
+  console.log("is-desktop");
+  resetMobileMenu();
+}
+
+ssm.addState({
+  id: "tablet",
+  query: "(max-width: 900px)",
+  onEnter: function () {
+    initMobile();
+  },
+});
+
+ssm.addState({
+  id: "desktop",
+  query: "(min-width: 900px)",
+  onEnter: function () {
+    initDesktop();
+  },
+});
+// ?END MOBILE Menu
 ////START validate email script
 const input = document.querySelector('.subscribe__email');
 document.addEventListener('submit', handleSubmit);
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -38,9 +38,12 @@ var swiperShedule = new Swiper('.swiperShedule', {
   },
 });
 
-var swiperShedule = new Swiper('.swiper_team', {
+var swiperTeamMob = new Swiper('.swiper_team-mob', {
   slidesPerView: 1,
-  slidesPerGroup: 5,
+  slidesPerGroup: 1,
+  
+  loop: true,
+  // enabled: false,
 
   // spaceBetween: 10,
   navigation: {
@@ -63,17 +66,65 @@ var swiperShedule = new Swiper('.swiper_team', {
       // spaceBetween: 20,
     },
     768: {
-      slidesPerView: 4,
+      slidesPerView: 3,
       // spaceBetween: 30,
     },
     1024: {
       slidesPerView: 3,
-      slidesPerGroup: 5,
-      grid: {
-        fill: 'row',
-        rows: 2,
-      },
-      // spaceBetween: 10,
+      slidesPerGroup: 3,
+      
+    
+    },
+  },
+});
+
+var swiperTeamBottom = new Swiper('.swiper_team_bottom', {
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+  speed: 1500,
+  loop: true,
+  allowTouchMove: false,
+});
+
+var swiperTeamTop = new Swiper('.swiper_team_top', {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  speed: 2500,
+  loop: true,
+  // enabled: false,
+
+  // spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination_team',
+    clickable: true,
+    bulletClass: 'swiper-pagination-bulet_team',
+    bulletActiveClass: 'swiper-pagination-bulet-active_team',
+    modifierClass: '_team',
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  },
+  thumbs: {
+    swiper: swiperTeamBottom,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      // spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      // spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      
+    
     },
   },
 });
